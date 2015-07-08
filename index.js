efd1841b898d2487cc04d3be1f7ffe86130fe88f
Jonathan Kingston <jonathan@jooped.co.uk>
2015-07-08 22:43:49 +0100

    Disable extension in development by default

--- index.js
+++ index.js
@@ -7,12 +7,26 @@ module.exports = {
     this._super.included.apply(this,arguments);
 
     this.options = app.options.SRI || {};
+
+    // SRI is default on
+    if (!('enabled' in this.options)) {
+      this.options.enabled = true;
+    }
+    // Development defaults to be off
+    if (app.env === 'development') {
+      this.options.enabled = false;
+    }
+
     if ('fingerprint' in app.options && 'prepend' in app.options.fingerprint) {
       this.options.prefix = app.options.fingerprint.prepend;
     }
   },
   postprocessTree: function(type, tree) {
     var options = this.options || {};
-    return require('broccoli-sri-hash')(tree, options);
+    if (options.enabled) {
+      return require('broccoli-sri-hash')(tree, options);
+    } else {
+      return tree;
+    }
   }
 };
