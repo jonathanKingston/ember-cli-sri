77b4197827923edf230cb6b67b4e124d0ceaeb12
Jonathan Kingston <jonathan@jooped.co.uk>
2015-07-18 13:45:45 +0100

    Adding in basic acceptance test for checking generated dummy output contains hashes

---
+++ app/acceptance-tests/main.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-cli-sri/acceptance-tests/main';
---
+++ tests/acceptance/main-test.js
@@ -0,0 +1,30 @@
+import Ember from 'ember';
+import { module, test } from 'qunit';
+import startApp from 'dummy/tests/helpers/start-app';
+
+var application;
+
+module('Acceptance | main', {
+  beforeEach: function() {
+    application = startApp();
+  },
+
+  afterEach: function() {
+    Ember.run(application, 'destroy');
+  }
+});
+
+test('visiting /main', function(assert) {
+  visit('/main');
+
+  andThen(function() {
+    var integrity = find('body').text();
+    var scripts = document.querySelectorAll('script,link');
+    [].forEach.call(scripts, function (script) {
+      var integrity = script.getAttribute('integrity');
+      assert.ok(/sha256[-]/.test(integrity), 'must have sha256');
+      assert.ok(/sha512[-]/.test(integrity), 'must have sha512');
+    });
+    
+  });
+});
